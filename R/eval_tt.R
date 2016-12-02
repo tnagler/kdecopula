7d53322a74e6c85d1096494954e74b7fd4f9b4fb
tnagler <thomas.nagler@tum.de>
2016-12-02 15:51:25 +0100

    roxygen doc for eval_tt

--- R/eval_tt.R
+++ R/eval_tt.R
@@ -1,20 +1,34 @@
-# The code is based on the Modified Transformation-Based Kernel Estimator (MTK) 
-# for copula densities, which is proposed in Wen and Wu (2015).
-# The code is written by Kuangyu Wen, Email: kweneco@gmail.com
-# Affiliation: International School of Economics and Management, Capital 
-# University of Economics and Business, Beijing, China
-
+#' Evaluation of tapered transformation estimator
+#' 
+#' This function returns copula density estimates
+#' given the bandwidth matrix 
+#' \deqn{H = h^2 * (1, \rho \\ \rho, 1)}
+#' and the additional tuning parameters \eqn{(\theta_1, \theta_2)}.  
+#'
+#' @param uev evaluation points.
+#' @param udata data.
+#' @param bw smoothing parameters, a numeric vector of length 4; entries are
+#' \code{c(h, rho, theta1, theta2)} using the notation of Wen and Wu (2015).
+#'
+#' @return copula density estimate evaluated at \code{uev}.
+#' 
+#' @details 
+#' The code is based on the Modified Transformation-Based Kernel Estimator (MTK) 
+#' for copula densities, which is proposed in Wen and Wu (2015).
+#' The code is written by Kuangyu Wen, Email: kweneco@gmail.com
+#' Affiliation: International School of Economics and Management, Capital 
+#' University of Economics and Business, Beijing, China
+#' 
+#' @author Kuangyu Wen
+#' 
+#' @references 
+#' Wen, K. and Wu, X. (2015).
+#' Transformation-Kernel Estimation of the Copula Density,
+#' Working paper,
+#' \url{http://agecon2.tamu.edu/people/faculty/wu-ximing/agecon2/public/copula.pdf}
+#' 
+#' @noRd
 eval_tt <- function(uev, udata, bw) {
-    # This function returns copula density estimates
-    # given the bandwidth matrix H = h^2 * (1, rho \\
-    # rho, 1) and the additional tuning parameters
-    # theta = (theta_1, theta_2).  udata is pseudo-data
-    # calculated from the rescaled rank of the orginal
-    # data; udata should be a matrix of dimension N*2.
-    # The first arguments of the evaluation points: u
-    # The second arguments of the evaluation points: v
-    # In this function, u and v should have the same
-    # length.
     u <- uev[, 1]
     v <- uev[, 2]
     h <- bw[1]
@@ -33,7 +47,7 @@ eval_tt <- function(uev, udata, bw) {
     eta <- mean(exp(-((4 * h^2 * theta[1]^2 - h^2 * 
                            theta[2]^2 + 2 * theta[1]) * (Si^2 + Ti^2) + 
                           (2 * rho * h^2 * theta[2]^2 - 8 * rho * h^2 * 
-                             theta[1]^2 + 2 * theta[2]) * Si * Ti)/2/delta^2))/delta
+                               theta[1]^2 + 2 * theta[2]) * Si * Ti)/2/delta^2))/delta
     
     arg1 <- outer(s, Si, function(p, q) (p - q)/h)
     arg2 <- outer(t, Ti, function(p, q) (p - q)/h)
@@ -43,6 +57,5 @@ eval_tt <- function(uev, udata, bw) {
         temp <- mean(temp) else temp <- rowMeans(temp)
     
     temp <- temp/h^2/eta
-    temp * exp(-theta[1] * (s^2 + t^2) - theta[2] * 
-                   s * t)/dnorm(s)/dnorm(t)
+    temp * exp(-theta[1] * (s^2 + t^2) - theta[2] * s * t)/dnorm(s)/dnorm(t)
 }
